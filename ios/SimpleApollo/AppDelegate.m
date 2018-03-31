723950e00be7a9c7ecc4c6b16b5d00f9361f0fa7
Wolfgang Mathurin <wmathurin@salesforce.com>
2018-03-30 18:22:13 -0700

    Adding store and sync configs

--- ios/SimpleApollo/AppDelegate.m
+++ ios/SimpleApollo/AppDelegate.m
@@ -75,6 +75,10 @@ static NSString * const OAuthRedirectURI        = @"testsfdc:///mobilesdk/detect
             //
 
             [SFSDKLogger log:[weakSelf class] level:DDLogLevelInfo format:@"Post-launch: launch actions taken: %@", [SalesforceSDKManager launchActionsStringRepresentation:launchActionList]];
+          
+            [((SmartSyncSDKManager*)[SalesforceSDKManager sharedManager]) setupUserStoreFromDefaultConfig];
+            [((SmartSyncSDKManager*)[SalesforceSDKManager sharedManager]) setupUserSyncsFromDefaultConfig];
+
             [weakSelf setupRootViewController];
         };
         [SalesforceSDKManager sharedManager].launchErrorAction = ^(NSError *error, SFSDKLaunchAction launchActionList) {
